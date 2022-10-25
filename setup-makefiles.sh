5e73431f43ef6932e3d8baaf0915142483b96f38
yogesh24ce071 <113939405+yogesh24ce071@users.noreply.github.com>
2022-10-25 05:40:23 +0530

    Update setup-makefiles.sh

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -8,8 +8,8 @@
 
 set -e
 
-export DEVICE=avicii
-export DEVICE_COMMON=sm7250-common
+export DEVICE=holi
+export DEVICE_COMMON=sm6375-common
 export VENDOR=oneplus
 
 "./../../${VENDOR}/${DEVICE_COMMON}/setup-makefiles.sh" "$@"
